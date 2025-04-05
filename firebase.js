b64c5bd546633e1dc5846e3250d392aeff32e6f3
nate8kake <nathanielle411@gmail.com>
2025-04-04 20:21:42 -0500

    hi firebase active

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwXWp1iEwUFyXs7LOxAcQ8P4rtjQMH3Eo",
+  authDomain: "terosmd.firebaseapp.com",
+  projectId: "terosmd",
+  storageBucket: "terosmd.firebasestorage.app",
+  messagingSenderId: "906566023341",
+  appId: "1:906566023341:web:8cae79c5d5afced684ae24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
