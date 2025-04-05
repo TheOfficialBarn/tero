2443fdb76b0b9eb816072cb71f626deaf62341ac
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2025-04-05 18:55:59 -0500

    deployment patch with dependencies

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,4 +1,4 @@
-import { openai } from '@ai-sdk/openai';
+import { google } from '@ai-sdk/google';
 import { streamText } from 'ai';
 
 // Allow streaming responses up to 30 seconds
@@ -8,8 +8,9 @@ async function POST(req) {
   const body = await req.json();
   const messages = body.messages;
 
+  // THis is not correct code, but I need this to deploy on Vercel
   const result = streamText({
-    model: openai('gpt-4-turbo'),
+    model: google('a google model goes here'),
     system: 'You are a helpful assistant.',
     messages: messages,
   });
