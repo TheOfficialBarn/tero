e113d43df6727a57a8c0666e609f5ca30045933f
flec1115 <balandranf11@gmail.com>
2025-04-04 22:22:08 -0500

    stuff

--- app/api/uploadToPinata/route.js
+++ app/api/uploadToPinata/route.js
@@ -42,10 +42,4 @@ export async function POST(request) {
   }
 }
 
-export async function GET(request) {
-  try {
-    const response = await fetch('https://api.pinata.cloud/data/pinList?status=pinned', {
-      headers: { Authorization: `Bearer ${process.env.PINATA_JWT}` }
-});
-  }
-}
+
