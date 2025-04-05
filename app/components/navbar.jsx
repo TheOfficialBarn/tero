c431808649c450583ce5985801cca37534c330af
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2025-04-04 21:42:49 -0500

    Refactoring; adding pages; working navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,25 +1,40 @@
+'use client';
+import Link from "next/link";
+
+const navItems = [
+	{path: '/', name: 'home'},
+	{path: '/records', name: 'records'}
+]
+
 export function NavBar() {
 	return (
 	  <div className="flex justify-center w-full">
-		  <nav
-		  className="
-			sm:hidden
-			fixed
-			bottom-5
-			w-4/5
-			bg-gradient-to-b from-gray-200 dark:from-gray-800 to-gray-600
-			rounded-4xl
-			p-4
-			flex
-			justify-center
-			gap-4
-			shadow-lg
-		  "
-		  >
-			  <button>Home</button>
-			  <button>Search</button>
-			  <button>Profile</button>
-		  </nav>
+		<nav
+		className="
+		sm:hidden
+		fixed
+		bottom-5
+		w-4/5
+		bg-gradient-to-b from-gray-200 dark:from-gray-800 to-gray-600
+		rounded-4xl
+		p-4
+		flex
+		justify-center
+		gap-4
+		shadow-lg
+		"
+		>
+		{navItems.map(({path, name}) => (
+			<Link
+			key={path}
+			href={path}
+			className="transition-all hover:text-neutral-300 flex align-middle relative"
+			>
+			{name}
+			</Link>
+		))}
+
+		</nav>
 	  </div>
 	);
   }
--- app/components/pfp.jsx
+++ app/components/pfp.jsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export function PFP() {
 	return (
 	  <div className="w-15 h-15 bg-white fixed top-15 right-3 rounded-full shadow-2xl flex items-center justify-center p-1">
-		<div className="w-full h-full rounded-full bg-black"></div>
+		<Link href="/login" className="w-full h-full rounded-full bg-black"></Link>
 	  </div>
 	)
   }
