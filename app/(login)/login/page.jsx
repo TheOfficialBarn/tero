d45e673597c49eb5c7fa370aa32b6f046dcc103c
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2025-04-06 06:12:34 -0500

    Everything looks crispy now

--- app/(login)/login/page.jsx
+++ app/(login)/login/page.jsx
@@ -257,7 +257,7 @@ export default function Page() {
 
 	return user ? (
 		<section className="h-screen flex justify-center items-center w-full">
-			<div className="bg-[rgba(130,130,130,0.5)] rounded-3xl p-6 flex flex-col items-center justify-center ">
+			<div className="bg-[rgba(130,130,130,0.5)] rounded-3xl p-6 flex flex-col items-center justify-center w-full">
 			<h3 className={`text-white text-3xl font-bold mb-4 text-center ${inter.variable}`}>Logged In 🥳</h3>
 			<button
 				onClick={handleLogout}
@@ -265,7 +265,7 @@ export default function Page() {
 			>
 				Logout
 			</button>
-			<p className="mt-4">Logged in as: {user.email}</p>
+			<p className="mt-4">Account: <code>{user.email}</code></p>
 			</div>
 		</section>
 	) : (
