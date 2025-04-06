b68737b1ae522a2f07801dec510fe998bad61428
nate8kake <nathanielle411@gmail.com>
2025-04-05 20:10:49 -0500

    route.js chat change

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -4,30 +4,18 @@ import { streamText } from 'ai';
 // Allow streaming responses up to 30 seconds
 const maxDuration = 30;
 
-// async function POST(req) {
-//   const body = await req.json();
-//   const messages = body.messages;
-
-//   const result = await streamText({
-//     model: google('gemini-1'),
-//     system: 'You are a helpful assistant.',
-//     messages: messages,
-//   });
-
-//   return result.toDataStreamResponse();
-// }
-
 async function POST(req) {
   const body = await req.json();
   const messages = body.messages;
 
-  const result = streamText({
-    model: openai('gpt-4-turbo'),
+  const result = await streamText({
+    model: google('gemini-1'),
     system: 'You are a helpful assistant.',
     messages: messages,
   });
 
   return result.toDataStreamResponse();
 }
+
 
 export { maxDuration, POST };
